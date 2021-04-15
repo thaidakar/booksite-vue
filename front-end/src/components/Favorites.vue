d783c8876aeff43b8fa0121d4ce601562d006513
thaidakar <77295925+thaidakar@users.noreply.github.com>
2021-04-14 22:39:10 -0600

    Update Favorites.vue

--- front-end/src/components/Favorites.vue
+++ front-end/src/components/Favorites.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="favorites">
     <h1>Favorite Books</h1>
-    <p v-if="this.$root.$data.user.username === ''">Please sign in to view or add to your favorite books</p>
+    <p v-if="this.$root.$data.user.username === ''">Please sign in to view and add to your favorite books</p>
     <p v-if="this.$root.$data.user.favorites.length === 0 && this.$root.$data.user.username !== ''">Currently no favorites, please select from the list to add some!</p>
     <div class="masonry" v-else>
         <div class="post item masonry-item" v-for="favorite in this.$root.$data.user.favorites" :key="favorite.name">
